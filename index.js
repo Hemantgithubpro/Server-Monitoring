e6fc48344a73817dc727eaf01192fa1451bce813
Hemant Jha <hemantjha2005@gmail.com>
2025-12-30 16:31:17 +0530

    fully working bucket list for grafana

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
-const client=require("prom-client"); // metric collection
+const client = require("prom-client"); // metric collection
 const { doSomeHeavyTask } = require("./util");
+const responseTime = require("response-time");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
 
-collectDefaultMetrics({register: client.register});
+collectDefaultMetrics({ register: client.register });
+
+const reqResTime = new client.Histogram({
+    name: "http_express_request_response_time",
+    help: "This tells how much time is taken by request and response cycle",
+    labelNames: ["method", "route", "status_code"],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 1.5, 2, 0.02] // in seconds
+});
+
+app.use(responseTime((req, res, time) => {
+    reqResTime.labels(
+        method = req.method,
+        route = req.url,
+        status_code = res.statusCode
+    ).observe(time);
+}));
+
 
 app.get("/", (req, res) => {
     res.send("Server is running");
